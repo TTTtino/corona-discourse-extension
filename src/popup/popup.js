7ca164d4bfc31bd73387d680b5854d2523efe5aa
psytt2 <psytt2@nottingham.ac.uk>
2021-09-10 15:47:03 +0100

    Improved popup - Visual changes - Icon turns b&w on disabling

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,8 +1,17 @@
 chrome.storage.local.get({extensionActive: false}, (result)=>{
     if(result.extensionActive){
+        document.getElementById("toggle-button").dataset.active = true;
         document.getElementById("toggle-button").innerHTML = "Active";
     } else{
+        document.getElementById("toggle-button").dataset.active = false;
         document.getElementById("toggle-button").innerHTML = "Not Collecting Data";
+        chrome.action.setIcon({
+            path: {
+                16: "/images/bw_logo16.png",
+                48: "/images/bw_logo48.png",
+                128: "/images/bw_logo128.png",
+            },
+        });
     }
 });
 
@@ -10,9 +19,18 @@ document.getElementById("toggle-button").addEventListener("click", () => {
     chrome.storage.local.get({extensionActive: false}, (result)=>{
         let status = !result.extensionActive;
         if(status){
+            document.getElementById("toggle-button").dataset.active = true;
             document.getElementById("toggle-button").innerHTML = "Active";
         } else{
+            document.getElementById("toggle-button").dataset.active = false;
             document.getElementById("toggle-button").innerHTML = "Not Collecting Data";
+            chrome.action.setIcon({
+                path: {
+                    16: "/images/bw_logo16.png",
+                    48: "/images/bw_logo48.png",
+                    128: "/images/bw_logo128.png",
+                },
+            });
         }
         chrome.storage.local.set({extensionActive: status}, ()=>{
         });
