727ac0e8d28f6bb8f3e882ff18ce0553e6ea8035
Miriam <miriam.aydt@nottingham.ac.uk>
2021-09-30 10:42:26 +0100

    Project class that contains basic info of selected project (id,name,description)

---
+++ src/ProjectInfo.js
@@ -0,0 +1,9 @@
+// Class representing info of  selected project
+
+class ProjectInfo{
+    constructor(id,name, description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
